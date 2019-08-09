4ba2641581cfe819458911f136f31771e6eb80c9
Rodion Kalyuzhin <tusur.rodion@gmail.com>
2019-08-09 17:32:43 +0700

    Sound added & frame check removed

--- Routine/main.js
+++ Routine/main.js
@@ -1,3 +1,11 @@
+var lastPlayedSound;
+
+Sound = {
+  on: new Sound("sounds/on.mp3", 100, false),
+  off: new Sound("sounds/off.mp3", 100, false),
+  swap: new Sound("sounds/swap.mp3", 100, false)
+}
+
 Mode = {
   Basic: 0,
   Kg: 1
@@ -74,13 +82,9 @@ function run() {
   counter = 0;
   $("#counter").css("color", "rgb(255, 159, 10)");
   setInterval(function() {
-    frame += 1;
     runChecks();
-    if (Math.random() > 0.4) {
-      frame = 0;
-      elapsedTime = (Date.now() - startTime) / 1000;
-      $("#timer").html(elapsedTime.toFixed(2));
-    }
+    elapsedTime = (Date.now() - startTime) / 1000;
+    $("#timer").html(elapsedTime.toFixed(2));
   }, 10);
 }
 
@@ -99,16 +103,63 @@ function runChecks() {
     default:
       console.log("Unknown run mode");
   }
+  if (state == State.Work && elapsedTime >= (workTime / 2.0)) {
+    if (lastPlayedSound != Sound.swap) {
+      Sound.swap.start();
+      lastPlayedSound = Sound.swap;
+    }
+  }
   if (state == State.Work && elapsedTime >= workTime) {
     counter += 1;
+    Sound.off.start();
+    lastPlayedSound= Sound.off;
     $("#counter").html(counter);
     setState(State.Rest);
     startTime = Date.now();
     elapsedTime = 0;
   }
   if (state == State.Rest && elapsedTime >= restTime) {
+    Sound.on.start();
+    lastPlayedSound= Sound.on;
     setState(State.Work);
     startTime = Date.now();
     elapsedTime = 0;
   }
+}
+
+
+function Sound(source, volume, loop)
+{
+    this.source = source;
+    this.volume = volume;
+    this.loop = loop;
+    var son;
+    this.son = son;
+    this.finish = false;
+    this.stop = function()
+    {
+        document.body.removeChild(this.son);
+    }
+    this.start = function()
+    {
+        if (this.finish) return false;
+        this.son = document.createElement("embed");
+        this.son.setAttribute("src", this.source);
+        this.son.setAttribute("hidden", "true");
+        this.son.setAttribute("volume", this.volume);
+        this.son.setAttribute("autostart", "true");
+        this.son.setAttribute("loop", this.loop);
+        document.body.appendChild(this.son);
+    }
+    this.remove=function()
+    {
+        document.body.removeChild(this.son);
+        this.finish = true;
+    }
+    this.init = function(volume, loop)
+    {
+        this.finish = false;
+        this.volume = volume;
+        this.loop = loop;
+    }
 }
